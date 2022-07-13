c5ebd406dd0f7f5b926dfb082214667290a2e89c
martin-k-kamir <martin.k.kamir@gmail.com>
2022-07-14 01:14:23 +0200

    add: typography

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 export default function App() {
 	return (
-		<div className="App">
-			<h2 className='f-size-10'>Lorem</h2>
-			<h2 className='f-size-9'>Lorem</h2>
-			<h2 className='f-size-8'>Lorem</h2>
-			<h2 className='f-size-7'>Lorem</h2>
-			<h2 className='f-size-6'>Lorem</h2>
-			<h2 className='f-size-5'>Lorem</h2>
-			<h2 className='f-size-4'>Lorem</h2>
-			<h2 className='f-size-3'>Lorem</h2>
-			<h2 className='f-size-2'>Lorem</h2>
-			<h2 className='f-size-1'>Lorem</h2>
+		<div className="container">
+			{/*<h2 className='title-big'>Lorem</h2>*/}
+			{/*<h1 className='title-1'>Lorem</h1>*/}
+			{/*<h2 className='title-2'>Lorem</h2>*/}
+			{/*<h3 className='title-3'>Lorem</h3>*/}
+			{/*<h4 className='title-4'>Lorem</h4>*/}
+			{/*<p className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi assumenda*/}
+			{/*	consequuntur facere,*/}
+			{/*	illo itaque labore laborum nihil placeat praesentium provident quae repellat sed, sequi suscipit totam*/}
+			{/*	voluptatibus. Deserunt ea facilis iste odit perspiciatis. Dolores eius eos eum facilis iure nam nulla*/}
+			{/*	quidem sequi, soluta!</p>*/}
+			{/*<p><span>menu</span> <span>home</span> <span>contact</span></p>*/}
+			{/*<button className='btn-primary' data-disabled={false}>button</button>*/}
 		</div>
 	);
 }
