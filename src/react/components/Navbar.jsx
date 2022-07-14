dccf7a2425ea16f26f45c6091b55ee52dbe33874
Martin Kamír <martin.k.kamir@gmail.com>
2022-07-14 17:50:34 +0200

    fix: generating mixins

--- src/react/components/Navbar.jsx
+++ src/react/components/Navbar.jsx
@@ -1,11 +1,11 @@
 export default function App() {
 	return (
 		<nav className="navbar">
-			<ul>
-				<li><a href="index.html">logo</a></li>
-				<li><a href="index.html">home</a></li>
-				<li><a href="index.html">about us</a></li>
-				<li><a href="index.html">create your plan</a></li>
+			<ul className="navbar__list">
+				<li className="navbar__item"><a href="index.html">logo</a></li>
+				<li className="navbar__item"><a href="index.html">home</a></li>
+				<li className="navbar__item"><a href="index.html">about us</a></li>
+				<li className="navbar__item"><a href="index.html">create your plan</a></li>
 			</ul>
 		</nav>
 	);
