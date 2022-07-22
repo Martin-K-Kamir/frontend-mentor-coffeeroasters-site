604879e7661fa3592ef977e3c257ae124928cbe1
martin-k-kamir <martin.k.kamir@gmail.com>
2022-07-22 09:04:20 +0200

    w

--- src/react/pages/Home.jsx
+++ src/react/pages/Home.jsx
@@ -11,33 +11,33 @@ export default function Home() {
 		<Cover coverImg={coverImg} hasBtn={true} href="/plan" title="Great coffee made simple." desc="Start your mornings with the world’s best coffees. Try our
 				expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule."/>
 
-		<main className="">
+		<main>
 			<section className="section--products">
 				<div className="container">
 					<h2 className="title-collection text-center">our collection</h2>
-					<div className="switcher space-6 measure-8">
-						{<Card data={data.products} type="1"/>}
+					<div className="wrap">
+						<div className="switcher space-6 measure-8">
+							{<Card data={data.products} type="1"/>}
+						</div>
 					</div>
 				</div>
 			</section>
 
 			<section className="section--features">
 				<div className="container half-surface text-neutral-1">
-					<div className="stack space-fluid-3">
+					<div className="stack">
 						<div className="wrap">
-							<div className="text-center">
-								<h2 className="title-3">Why choose us?</h2>
-								<p className="description inline-center space-fluid-2 measure-5">
-									A large part of our role is choosing which particular coffees will be featured
-									in
-									our
-									range.
-									This means working closely with the best coffee growers to give you a more
-									impactful
-									experience
-									on every level.
-								</p>
-							</div>
+							<h2 className="title-3 text-center">Why choose us?</h2>
+							<p className="description text-center inline-center measure-5">
+								A large part of our role is choosing which particular coffees will be featured
+								in
+								our
+								range.
+								This means working closely with the best coffee growers to give you a more
+								impactful
+								experience
+								on every level.
+							</p>
 						</div>
 						<div className="wrap">
 							<div className="switcher space-3 measure-8">
@@ -51,9 +51,10 @@ export default function Home() {
 
 			<section className="section--steps">
 				<div className="container">
-					<div className="stack space-5">
+					<div className="stack">
 						<h2 className="title-4 text-neutral-4">How it works</h2>
 						<Steps length={data.steps}/>
+
 						<div className="wrap">
 							<div className="switcher space-5 measure-6">
 								{<Card data={data.steps} type="3"/>}
