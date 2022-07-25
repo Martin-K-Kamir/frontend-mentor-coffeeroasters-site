f7052ca6ec9a4f25c8cac249fc61de6fa4588dbf
Martin Kamír <martin.k.kamir@gmail.com>
2022-07-25 18:02:42 +0200

    w

---
+++ src/react/components/OrderSummary.jsx
@@ -0,0 +1,11 @@
+export default function OrderSummary() {
+
+	return (<div className="order-summary radius-3">
+		<h2 className="order-summary__title font-sans-serif">Order Summary</h2>
+		<p className="order-summary__description font-serif">
+			<blockquote>I drink my coffee using Capsules, with a Decaf type of bean. 250g , sent to me Every
+				Week.
+			</blockquote>
+		</p>
+	</div>);
+}
