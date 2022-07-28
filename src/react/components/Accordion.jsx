5aa447f73dabb10eab53faf5d6bc0f493570b40f
martin-k-kamir <martin.k.kamir@gmail.com>
2022-07-28 02:06:40 +0200

    w

--- src/react/components/Accordion.jsx
+++ src/react/components/Accordion.jsx
@@ -1,54 +1,47 @@
 import React, {useState} from "react";
 
 export default function Accordion(props) {
-	const [isOpen, setIsOpen] = useState({
-		preferences: true,
-	});
-
+	const [isOpen, setIsOpen] = useState({preferences: true});
 	const [isChecked, setIsChecked] = useState({});
-	const [card, setCard] = useState(null);
 
-	function toggle(i, e) {
-		const clickOn = e.currentTarget.getAttribute('data-click');
-
-		if (clickOn === 'card') {
-			setIsOpen({...isOpen, [i]: true});
-		}
-		if (clickOn === 'button') {
-			setIsOpen({...isOpen, [i]: !isOpen[i]});
-		}
+	function handleOpening(id) {
+		setIsOpen({...isOpen, [id]: !isOpen[id]});
 	}
 
-	function handleCheck(i, index) {
-		setIsChecked({...isChecked, [i.key]: i.item[index].key});
-
+	function handleChecking(curGroup, i) {
+		setIsOpen({...isOpen, [curGroup.item[i].ref]: true});
+		setIsChecked({...isChecked, [curGroup.key]: curGroup.item[i].key});
 	}
 
-
-	function renderItems(curGroup) {
-		return (curGroup.item.map((curItem, index) => (
-			<li className="accordion__item" key={curItem.key} onClick={(e) => (toggle(curItem.ref, e), handleCheck(curGroup, index))} data-click="card"
-			    data-checked={isChecked[curGroup.key] == curItem.key} value={curItem.key}>
-				<a href={`#${curItem.ref}`} className="accordion__card space-3 radius-3">
+	function renderCards(curGroup) {
+		return (curGroup.item.map((curItem, i) => (
+			<li key={curItem.key} onClick={() => handleChecking(curGroup, i)}>
+				<a className="card2 space-3 radius-3" href={`#${curItem.ref}`}
+				   data-checked={isChecked[curGroup.key] === curItem.key}>
 					<h3 className="title-4 f-weight-3">{curItem.title}</h3>
 					<p className="description space-1">{curItem.desc}</p>
 				</a>
-			</li>)))
+			</li>
+		)))
 	}
 
-	return (<form action="#" className="accordion stack space-7">
-		{props.data.map(curGroup => (<div className="accordion__group" id={curGroup.id} key={curGroup.key}>
-			<header className="accordion__header" onClick={(e) => toggle(curGroup.id, e)} data-click="button">
-				<h2 className="title-2 text-neutral-4">{curGroup.title}</h2>
-				<svg className="accordion__arrow" data-open={isOpen[curGroup.id]} width="24" height="24"
-				     viewBox="0 0 19 11">
-					<use href="assets/sprites.svg#arrow"/>
-				</svg>
-			</header>
-			<ul className="accordion__list switcher measure-6 space-2"
-			    data-open={isOpen[curGroup.id] ?? false}>
-				{renderItems(curGroup)}
-			</ul>
-		</div>))}
-	</form>);
+	return (
+		<form action="#" className="accordion stack space-7">
+			{props.data.map(curGroup => (
+				<div className="accordion__group" id={curGroup.id} key={curGroup.key}>
+					<h2 className="accordion__header title-2 text-neutral-4" onClick={() => handleOpening(curGroup.id)}>
+						<button>{curGroup.title}</button>
+						<svg className="accordion__arrow" data-open={isOpen[curGroup.id]} width="24" height="24"
+						     viewBox="0 0 19 11">
+							<use href="assets/sprites.svg#arrow"/>
+						</svg>
+					</h2>
+					<ul className="accordion__list switcher measure-6 space-2"
+					    data-open={isOpen[curGroup.id] ?? false}>
+						{renderCards(curGroup)}
+					</ul>
+				</div>
+			))}
+		</form>
+	);
 }
