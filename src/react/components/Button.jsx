b91cf695d65966029cab7b741eb0dacf3308542c
martin-k-kamir <martin.k.kamir@gmail.com>
2022-08-02 00:32:34 +0200

    w

--- src/react/components/Button.jsx
+++ src/react/components/Button.jsx
@@ -3,9 +3,9 @@ export default function Button(props) {
 	return (
 		<>
 			{props.href ?
-				<a href={props.href} className="btn-primary" data-disabled={props.disabled}>{props.content}</a>
+				<a href={props.href} className={props.secondary ? "btn-secondary" : "btn-primary"} data-disabled={props.disabled}>{props.content}</a>
 				:
-				<button className="btn-primary" data-disabled={props.disabled}>{props.content}</button>}
+				<button className={props.secondary ? "btn-secondary" : "btn-primary"} data-disabled={props.disabled}>{props.content}</button>}
 		</>
 	);
 }
--- src/react/components/Modal.jsx
+++ src/react/components/Modal.jsx
@@ -1,27 +1,27 @@
 import OrderSummary from "./OrderSummary";
 import Button from "./Button";
-import {useWindowDimensions} from "../Hooks";
 
 import React from "react";
 
 
 export default function Modal(props) {
-	const {height, width} = useWindowDimensions();
 
 	return (
 		<div className="modal">
 			<div className="modal__container">
-				<header className="modal__header">
-					<h2 className="title-4 text-neutral-1">Order Summary</h2>
-				</header>
-				<div className="modal__body stack space-7">
-					<OrderSummary order={props.order}/>
-					<p className="description">Is this correct? You can proceed to checkout or go back to
-						plan selection if something is off. Subscription discount codes can also be redeemed at the
-						checkout. </p>
-					<div className="modal__cta space-3">
-						<p className="title-4">$14.00 / mo</p>
-						<Button content={width > 600 ? "Checkout" : "Checkout - $14.00 / mo"}/>
+				<div className="modal__body">
+					<header className="modal__header">
+						<h2 className="title-4 text-neutral-1">Order Summary</h2>
+					</header>
+					<div className="modal__content stack space-7">
+						<OrderSummary order={props.order}/>
+						<p className="description">Is this correct? You can proceed to checkout or go back to
+							plan selection if something is off. Subscription discount codes can also be redeemed at the
+							checkout. </p>
+						<div className="modal__cta   space-3">
+							<Button secondary={true} content="Go back"/>
+							<Button content="Checkout - $14.00 / mo"/>
+						</div>
 					</div>
 				</div>
 			</div>
