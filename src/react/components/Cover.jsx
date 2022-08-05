eac535f4ca2c43ce1a3d4b8f372847046955feb7
martin-k-kamir <martin.k.kamir@gmail.com>
2022-08-05 20:28:46 +0200

    w

--- src/react/components/Cover.jsx
+++ src/react/components/Cover.jsx
@@ -5,7 +5,7 @@ export default function Cover(props) {
 	const coverImage = `../assets/${props.coverImage}/image-${props.coverImage}-cover.${isWebpSupported() ? 'webp' : 'jpg'}`;
 
 	return (
-		<div className="cover container box darken-image" role="banner"
+		<div className="cover container box darken-image"
 		     style={{backgroundImage: `url(${coverImage})`}}>
 			<h1 className="title-1 text-neutral-1">{props.title}</h1>
 			<p className="description text-neutral-1">{props.desc}</p>
